7786dd0e92251a05d2507c6f30c401308d2c0264
RosarioMazzocca <mazzocca.rosario@libero.it>
2021-11-12 14:16:51 +0100

    Primo commit

---
+++ src/org/generation/italy/StampaSaluto.java
@@ -0,0 +1,12 @@
+package org.generation.italy;
+
+public class StampaSaluto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ciao prima stampa con Java");
+		// out sta ad indicare l'output e println significa print line
+
+	}
+
+}
